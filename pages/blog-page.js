1420663fd39d11675f17424b6d58dd369b76255a
Shin Tako <k1x.heart@icloud.com>
2021-07-02 02:48:31 +0900

    Changed to function

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -2,17 +2,16 @@ import Layout from "../components/Layout"
 import { getAllPostsData } from "../lib/posts";
 import Post from "../components/Post";
 
-const Blog = ({ posts }) => {
+export default function Blog({ posts }) {
   return (
     <Layout title='Blag'>
       <ul className='m-10'>
         {posts && posts.map((post) => <Post key={post.id} post={post} />)}
       </ul>
     </Layout>
-  )
-};
+  );
+}
 
-export default Blog
 
 export async function getStaticProps() {
   const posts = await getAllPostsData();
